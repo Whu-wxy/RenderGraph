b75d214c469c860652b2861b877af24195a582a0
Wang Xinyu <641645560@qq.com>
2024-01-07 00:07:48 +0800

    增加编组和校验

--- read_xls_nodes.py
+++ read_xls_nodes.py
@@ -1,38 +1,64 @@
 import pandas as pd
 
 
-def read_map_xls(xls_path, node_idx, link_idx, comment_idx=None, seperator=',', row_start_idx=0):
+def read_map_xls(xls_path, node_idx, link_idx, comment_idx=None, group_idx=None, seperator=',', row_start_idx=0):
     df = pd.read_excel(xls_path, header=None) # usecols=[node_idx, link_idx],
-    # print(df)
     nodes_data = df.iloc[row_start_idx:, node_idx].tolist()
     links_data = df.iloc[row_start_idx:, link_idx].tolist()
     if comment_idx is not None:
-        comment_data = df.iloc[:, comment_idx].tolist()
+        comment_data = df.iloc[row_start_idx:, comment_idx].tolist()
+    if group_idx is not None:
+        group_data = df.iloc[row_start_idx:, group_idx].tolist()
 
     nodes = []
     links = []
-    global_dict = {}
+    global_link_dict = {}
     nodes_set = set()
+    if group_idx is not None:
+        grp_set = set(group_data)
+        for nd in group_data:
+            if nd not in nodes_set:
+                nodes.append({'name': nd, 'category': nd})
+            nodes_set.add(nd)
+    else:
+        grp_set = set()
+
     for i, nd in enumerate(nodes_data):
-        if comment_idx is not None:
-            nodes.append({'name': nd, 'value': comment_data[i]})
-        else:
-            nodes.append({'name': nd})
+        assert nd not in nodes_set, '节点定义重复:{}, 行数:{}'.format(nd, i)
+        assert nd != 'nan', '节点名字未定义:{}, 行数:{}'.format(nd, i)
 
         nodes_set.add(nd)
-        prev_node = links_data[i]
-        for prev_nd in str(prev_node).split(seperator):
-            links.append({'target': nd, 'source': prev_nd})
-            global_dict[prev_nd] = nd
-            if prev_nd not in nodes_set:
-                nodes.append({'name': prev_nd})
+        prop = {}
+        prop['name'] = nd
+        if comment_idx is not None:
+            prop['value'] = comment_data[i]
+        if group_idx is not None:
+            prop['category'] = group_data[i]
+
+        nodes.append(prop)
+
+    for i, prev_nodes in enumerate(links_data):
+        for prev_nd in str(prev_nodes).split(seperator):
+            target_nd = nodes_data[i]
+
+            if prev_nd != 'nan':
+                assert prev_nd in nodes_set, '前置节点未定义: {}, 行数:{}'.format(prev_nd, i)
+            else:
+                if group_idx is not None:
+                    links.append({'target': target_nd, 'source': group_data[i]})
+                    continue
+
+            links.append({'target': target_nd, 'source': prev_nd})
+            global_link_dict[prev_nd] = target_nd
 
     # print(nodes)
     # print(links)
     # print(global_dict)
 
-    return nodes, links
+    return nodes, links, global_link_dict, nodes_set, grp_set
 
 
 if __name__ == '__main__':
-    read_map_xls('test.xls', 0, 2, 3)
+    nodes, links = read_map_xls('entegor.xls', 0, 2, 3, row_start_idx=1)
+    print(nodes)
+    print(links)
--- render_entegor.py
+++ render_entegor.py
@@ -9,8 +9,9 @@ import yaml
 yamlPath = 'config.yaml'
 
 
-def render_entegor(xls_path, node_idx, link_idx, comment_idx=None, seperator=',', row_start_idx=0):
-    nodes, links = read_map_xls(xls_path, node_idx, link_idx, comment_idx, seperator, row_start_idx)
+def render_entegor(xls_path, node_idx, link_idx, comment_idx=None, group_idx=None, seperator=',', row_start_idx=0):
+    nodes, links, _, __, grp_set = read_map_xls(xls_path, node_idx, link_idx, comment_idx, group_idx, seperator, row_start_idx)
+    categories = [opts.GraphCategory(name=grp) for grp in grp_set]
 
     # init_opts=opts.InitOpts(width='100%', height='100%')
     G = Graph(init_opts=opts.InitOpts(width='100%', height='1000px', renderer='svg', theme=ThemeType.ESSOS, animation_opts=opts.AnimationOpts(animation=False)))
@@ -19,21 +20,41 @@ def render_entegor(xls_path, node_idx, link_idx, comment_idx=None, seperator=','
         nodes=nodes,
         symbol_size=50,
         links=links,
+        categories=categories,
         repulsion=1000,  # 斥力
         # edge_label=opts.LabelOpts(is_show=True, position='middle', formatter='{b}的数据{c}'),
-        symbol='rect',
+        symbol='roundRect',  # arrow pin roundRect
         is_draggable=True,
         friction=0.4,
         is_rotate_label=False,
         is_layout_animation=True,
         edge_symbol=['','arrow']
     )
-    G.set_global_opts(title_opts=opts.TitleOpts(title='Entegor调度表Demo'))
+
+    G.set_global_opts(title_opts=opts.TitleOpts(title='Entegor调度表Demo'),
+                      toolbox_opts=opts.ToolboxOpts(is_show=True,
+                                                    pos_top="top",
+                                                    pos_left="right",
+                                                    feature={"saveAsImage": {},
+                                                             "dataZoom": {},
+                                                             "restore": {},
+                                                             "magicType": {"show": False, "type": ["line", "bar"]},
+                                                             "dataView": {},
+                                                             "brush": {}
+                                                             }
+                                                    ),
+                      brush_opts=opts.BrushOpts(tool_box=['rect', 'polygon', 'lineX', 'lineY', 'keep', 'clear'],
+                                                brush_link='all'),
+                      datazoom_opts=opts.DataZoomOpts()
+                      )
+    G.set_colors(['#6a89cc', '#38ada9', '#f6b93b', '#f8c291', '#c9f29b', '#7bed9f', '#ff6b81', '#70a1ff', '#747d8c', '#574b90',
+                  '#c44569', '#3B3B98', '#82589F', '#55E6C1', '#CAD3C8', '#F97F51', '#58B19F', '#191970', '#0ABAB5', '#32CD32'])
     G.render(path='render.html')
     html_to_share('render.html', 'echarts.min.js')
     os.system("render.html")
 
 if __name__ == '__main__':
+    print('begin render...')
     assert os.path.exists(yamlPath), "config.yaml配置文件不存在"
 
     cfs = {}
@@ -41,8 +62,20 @@ if __name__ == '__main__':
         cfs = f.read()
         cfs = yaml.load(cfs, Loader=yaml.Loader)
 
-    target_xls, node_idx, link_idx, comment_idx, seperator, row_start_idx = cfs['target_xls'], cfs['node_idx'], cfs['link_idx'], cfs['comment_idx'], cfs['seperator'], cfs['row_start_idx']
+    target_xls, \
+    node_idx, \
+    link_idx, \
+    comment_idx, \
+    group_idx, \
+    seperator, \
+    row_start_idx = \
+        cfs['target_xls'], \
+        cfs['node_idx'], \
+        cfs['link_idx'], \
+        cfs['comment_idx'], \
+        cfs['group_idx'], \
+        cfs['seperator'], \
+        cfs['row_start_idx']
 
     assert os.path.exists(target_xls), "目标xls文件不存在"
-    render_entegor(target_xls, node_idx, link_idx, comment_idx, seperator, row_start_idx)
-
+    render_entegor(target_xls, node_idx, link_idx, comment_idx, group_idx, seperator, row_start_idx)
